ec09c07b5c19dfb4a9dd811ffdd4938a10cf6636
Egorik <edcftiz2@gmail.com>
2026-02-15 20:07:20 +0300

    first commit

---
+++ api/proxy.js
@@ -0,0 +1,32 @@
+const fetch = require('node-fetch');
+
+module.exports = async (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+    
+    if (req.method === 'OPTIONS') {
+        res.status(200).end();
+        return;
+    }
+
+    const { url } = req.query;
+    if (!url) {
+        return res.status(400).send('Missing url parameter');
+    }
+
+    try {
+        const response = await fetch(url, {
+            headers: {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+                'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+                'Referer': 'https://learn.microsoft.com/',
+            }
+        });
+        
+        const text = await response.text();
+        res.send(text);
+    } catch (error) {
+        res.status(500).send(error.toString());
+    }
+};
